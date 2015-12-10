c6be7ef1fbf827046cbfcf967261155b455552e7
Marco S <marco.schriek@live.nl>
2015-12-10 10:40:04 +0100

    Basis

---
+++ Game/Assets/Scripts/Interface Scripts/TestButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : MonoBehaviour {
+
+	//GEBRUIK DIT SCRIPT NIET, PAS UM OOK NIET AAN ENZO
+
+	//Dit script is een voorbeeld om een functie te koppelen aan interface (UI) buttons
+	//Het hangt aan 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Test()
+	{
+		Debug.Log ("TEST");
+	}
+}
